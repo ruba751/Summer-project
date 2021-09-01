720baad578e19683cd9377e5dcc3a344baec5cca
ruba <rubarush1@gmail.com>
2021-09-01 23:53:07 +0300

    first test1

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import detectExit
 import movment
 import moveToExit
-import detectExit
 
 land_deg=movment.take_shoots()
 print('wait for orb_slam and the drone then enter fly....')
 x=input()
-while x!='fly':    # waiting for orbslam work to be finished - then we will press fly to do the processing on the data
-    print('wait')
-    x=input()
+while x!='fly':
+     print('wait')
+     x=input()
 print(land_deg)
-x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle = detectExit.mainyy()
+x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle = detectExit.mainyy()     #after getting the data we will manupilate the points-cleaning #points within the room ...
+print(quarterIndex)
 moveToExit.move(land_deg, x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle)
+
--- moveToExit.py
+++ moveToExit.py
@@ -3,21 +3,22 @@ from movment import Drone
 
 
 def move(distAngle, x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle):
-   
+
     Drone.takeoff()
     Drone.move_up(60)
 
+    print(Drone.get_battery())
+    if distAngle<0:
+        Drone.rotate_clockwise(abs(distAngle))
+    else:
+        Drone.rotate_counter_clockwise(distAngle)
     if(quarterIndex == 1):
-        Drone.rotate_clockwise(angle-distAngle)
+        Drone.rotate_clockwise(90-angle)
     elif(quarterIndex == 2):
-        Drone.rotate_clockwise(90-distAngle)
-        Drone.rotate_clockwise(abs(angle))
+        Drone.rotate_clockwise(90-angle)
     elif(quarterIndex == 3):
-        Drone.rotate_counter_clockwise(distAngle)
-        Drone.rotate_counter_clockwise(90)
-        Drone.rotate_counter_clockwise(angle)
+        Drone.rotate_counter_clockwise(90+angle)
     elif(quarterIndex == 4):
-        Drone.rotate_counter_clockwise(distAngle)
-        Drone.rotate_counter_clockwise(90-angle)
-
+        Drone.rotate_counter_clockwise(90+angle-8)     # a constant value - for errors
+# rotating the drone angle acordingly
     detectExit.moveDrone()
