5de741c6c7a1417296fe82b769d450a931063073
ruba <rubarush1@gmail.com>
2021-09-01 23:39:51 +0300

    first test1

--- movment.py
+++ movment.py
@@ -4,30 +4,30 @@ from time import sleep
 from djitellopy import tello
 import os
 
-Drone = tello.Tello();
-Drone.connct();
-
+Drone = tello.Tello()
+Drone.connect()
+print(Drone.get_battery())
 def boot_orbSlam():        # this function will make the orbSlam start working on the drone cam input
     os.chdir('/home/ruba/ORB_SLAM2')
     OrbSlamBoot = './Examples/Monocular/mono_tum Vocabulary/ORBvoc.txt Examples/Monocular/TUM1.yaml'
     os.system(OrbSlamBoot)
 
 
-def take_shoots():         # this function will activate orb slam and start reconrding the room from diffrent y axis values and angles 
-    Drone.streamoff()      # resetting previous connections 
+def take_shoots():         # this function will activate orb slam and start reconrding the room from diffrent y axis values and angles
+    Drone.streamoff()      # resetting previous connections
     Drone.streamon()
     run_Orbslam = threading.Thread(target=boot_orbSlam)
     run_Orbslam.start()
 
     # drone takes off and scans the room
     Drone.send_rc_control(0, 0, 0, 0)
-    sleep(1)           
+    sleep(1)
     Drone.takeoff()
-    Drone.move_up(80)
+    Drone.move_up(50)
     sleep(3)
     angle = 0
-    while(angle!=360): 
-        Drone.rotate_clockwise(20)
+    while(angle!=360):                # the drone will fly up and down scanning the 
+        Drone.rotate_clockwise(20)    # room
         sleep(4)
         Drone.move_up(25)
         sleep(4)
@@ -36,6 +36,6 @@ def take_shoots():         # this function will activate orb slam and start reco
         angle+=20
     Drone.land()
     Drone.streamoff()
-    landing_yaw_degree = Drone.get_yaw()     # need this data inorder to ??????offset the starting angle - the drone doensn't land exactly where it tookoff
+    landing_yaw_degree = Drone.get_yaw()     # need this data inorder to reset the starting angle - the drone doensn't land exactly where it tookoff
     run_Orbslam.join()                       # quarantee that orbslam is shut down
-    return landing_yaw_degree 
+    return landing_yaw_degree
