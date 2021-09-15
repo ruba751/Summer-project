c63e11540c23cb1dcf20f98f80e713676ae98d3c
Ruba Rushrush <70092224+ruba751@users.noreply.github.com>
2021-09-16 00:35:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import detectExit
 import movment
 import moveToExit
 
-land_deg=movment.take_shoots()
+land_deg=movment.take_shoots()                   #here we will get data into orb-slam2 by flying the drone up and down rotating 360 degree - screening the whole room
 print('wait for orb_slam and the drone then enter fly....')
-x=input()
+x=input()                                        # we will wait untill orb-slam2 finishes his work, then we have to press "fly" - to countinue 
 while x!='fly':
      print('wait')
      x=input()
 print(land_deg)
-x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle = detectExit.mainyy()     #after getting the data we will manupilate the points-cleaning #points within the room ...
+x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle = detectExit.mainyy()       #after getting the data we will manupilate the points-cleaning #points within the room ...
 print(quarterIndex)
 
-moveToExit.move(land_deg, x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle)
+moveToExit.move(land_deg, x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle)  #and finally we will move the drone towards the exit point we found previously
 
