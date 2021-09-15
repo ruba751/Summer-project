55efc57c901554004598b8d845ba0abed7fa85fb
Ruba Rushrush <70092224+ruba751@users.noreply.github.com>
2021-09-16 01:38:23 +0300

    Update moveToExit.py

--- moveToExit.py
+++ moveToExit.py
@@ -2,14 +2,13 @@ import detectExit
 from movment import Drone
 
 
-def move(distAngle, x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle):
+def move(distAngle, x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterIndex, angle):    #we will rotate the drone to an angle which it will fly in a direct way from it's location to the exit point of the room
 
     Drone.takeoff()
     Drone.move_up(60)
 
-    print(Drone.get_battery())
     if distAngle<0:
-        Drone.rotate_clockwise(abs(distAngle))
+        Drone.rotate_clockwise(abs(distAngle))  #dist angle is the error angle- at first we concidered the start angle as our origin but after flying 360 degree and scanning the room- the drone won't land on the origin point, so we will calculate the diffrence between the origin and the land angle degree and thus make things right
     else:
         Drone.rotate_counter_clockwise(distAngle)
     if(quarterIndex == 1):
@@ -21,5 +20,5 @@ def move(distAngle, x, y, z, mideanZ, mideanX, rectangle, xNew, zNew, quarterInd
     elif(quarterIndex == 4):
         Drone.rotate_counter_clockwise(90+angle-8)     # a constant value - for errors
 
-# rotating the drone angle acordingly
+# rotating the drone angle acordingly and then fly in a direct way
     detectExit.moveDrone()
